771fd3775ee5fcb4ebabd868841b00c74fc956bd
Tor Shimizu <torshimizu@gmail.com>
2018-07-05 16:07:46 -0700

    started git

---
+++ Assets/Unity Sample UI/Scripts/characterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    public float moveSpeed = 80f;
+    public float turnSpeed = 80f;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+		
+        if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);         
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);         
+        }
+
+	}
+}
---
+++ Assets/Unity Sample UI/Scripts/popUpClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popUpClick : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public GameObject canvasObj;
+
+    private void OnMouseDown() {
+        
+        // activate the canvas with the chamorro word text
+        canvasObj = this.gameObject.transform.GetChild(0).gameObject;
+        bool isActive = canvasObj.activeSelf;
+        canvasObj.SetActive(!isActive);
+    }
+}
