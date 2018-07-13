ad578704e8a28be2bdbd7607accac31dbab3f375
Tor Shimizu <torshimizu@gmail.com>
2018-07-12 21:58:11 -0700

    Reset Event Listener in Explore Mode

---
+++ Assets/Scripts/ExploreMode/PopUpClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpClick : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            EventManager.TriggerEvent("test");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        
+        GameObject canvasObj = this.gameObject.transform.Find("PopUp").gameObject;
+        bool isActive = canvasObj.activeSelf;
+        canvasObj.SetActive(!isActive);
+
+        EventManager.TriggerEvent("test2");
+    }
+
+}
