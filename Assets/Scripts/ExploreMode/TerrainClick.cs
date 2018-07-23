bf7091c915e46c7034031067c2400d3751855671
Tor Shimizu <torshimizu@gmail.com>
2018-07-23 13:21:15 -0700

    merged into master and added click to sand

---
+++ Assets/Scripts/ExploreMode/TerrainClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerrainClick : MonoBehaviour
+{
+    public GameObject popUp;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Vector3 mousePosition = Input.mousePosition;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(mousePosition), out hit, 100))
+            {
+                Debug.Log("Clicked the terrain");
+                bool isActive = popUp.activeSelf;
+                popUp.SetActive(!isActive);
+                if (!isActive)
+                {
+                    string translation = this.GetComponentInChildren<Text>().text;
+
+                    Vector3 canvasPosition = new Vector3(hit.point.x, hit.point.y + 8.0f, hit.point.z);
+                    popUp.transform.position = canvasPosition;
+                    EventManager.TriggerEvent("popUp", "Sand", translation);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/ExploreMode/characterController.cs
+++ Assets/Scripts/ExploreMode/characterController.cs
@@ -14,20 +14,29 @@ public class characterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)){
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.S)) {
-			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
-		
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A)){
 			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);         
         }
-
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);         
+        }
+        if (Input.GetKey(KeyCode.D)){
 			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);         
+        }
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);         
         }
 
 	}
